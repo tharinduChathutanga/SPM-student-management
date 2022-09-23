2f7cb3faba1eff5c303a3873131dcb85cbf46aa7
Pragathika <pragathikasupi@gmail.com>
2022-09-23 13:37:18 +0530

    Notice function - delete operation completed in backend

--- backend/routes/noticeRoutes.js
+++ backend/routes/noticeRoutes.js
@@ -3,22 +3,22 @@ const Posts = require('../models/noticeModels');
 
 const router = express.Router();
 
-//Save posts
+//Save posts (Save notices)
 
-router.post('/postNotice/save',(req,res) =>{
+router.post('/postNotice/save', (req, res) => {
 
     let newPost = new Posts(req.body);
 
-    newPost.save((err) =>{
+    newPost.save((err) => {
 
-        if(err){
+        if (err) {
             return res.status(400).json({
-                error:err
+                error: err
             });
 
         }
         return res.status(200).json({
-            success:"Class Details Saved Successfully"
+            success: "Class Details Saved Successfully"
         });
     });
 
@@ -26,33 +26,33 @@ router.post('/postNotice/save',(req,res) =>{
 
 //get posts( All Notices)
 
-router.get('/postsNotice',(req,res) =>{
-    Posts.find().exec((err,posts) =>{
-        if(err){
+router.get('/postsNotice', (req, res) => {
+    Posts.find().exec((err, posts) => {
+        if (err) {
             return res.status(400).json({
-                error:err
+                error: err
             });
         }
         return res.status(200).json({
-            success:true,
-            existingPosts:posts
+            success: true,
+            existingPosts: posts
         });
     });
 });
 
 //get a specific post (Get specific notice)
 
-router.get("/postNotice/:id",(req,res) =>{
+router.get("/postNotice/:id", (req, res) => {
 
-    let postId  = req.params.id;
+    let postId = req.params.id;
 
-    Posts.findById(postId,(err,post) =>{
-        if(err){
-            return res.status(400).json({success:false, err});
+    Posts.findById(postId, (err, post) => {
+        if (err) {
+            return res.status(400).json({ success: false, err });
         }
 
         return res.status(200).json({
-            success:true,
+            success: true,
             post
         });
     });
@@ -60,34 +60,34 @@ router.get("/postNotice/:id",(req,res) =>{
 
 //update posts (Update notices)
 
-router.put('/postNotice/update/:id',(req,res) =>{
+router.put('/postNotice/update/:id', (req, res) => {
     Posts.findByIdAndUpdate(
         req.params.id,
         {
-            $set:req.body
+            $set: req.body
         },
-        (err,post) =>{
-            if(err){
-               return res.status(400).json({error:err});
+        (err, post) => {
+            if (err) {
+                return res.status(400).json({ error: err });
             }
 
             return res.status(200).json({
-                success:"Updated Successfully"
+                success: "Updated Successfully"
             });
-        });    
-    });
+        });
+});
 
-//delete post
+//delete post (Delete specific notice )
 
-router.delete('/postNotice/delete/:id',(req,res) =>{
-    Posts.findByIdAndRemove(req.params.id).exec((err,deletedPost) =>{
+router.delete('/postNotice/delete/:id', (req, res) => {
+    Posts.findByIdAndRemove(req.params.id).exec((err, deletedPost) => {
 
-        if(err) return res.status(400).json({
-            message:"Delete unsuccessfull",err
+        if (err) return res.status(400).json({
+            message: "Delete unsuccessfull", err
         });
 
         return res.json({
-            message:"Delete successfull",deletedPost
+            message: "Delete successfull", deletedPost
         });
     });
 });
